c8bb202e2aaec802e16e8175b76a6578a8e023e1
Prakash Kumar <34745120+prakashk0301@users.noreply.github.com>
2023-09-21 23:53:35 +0530

    Update var.tf

---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "aws_key" {
+    description = "aws key"
+    default = "aws-frankfurt"
+}
+
+ variable "aws_ami" {
+     description = "ami id"
+     default = "ami-04e601abe3e1a910f"
+}
+
+ variable "instance_type" {
+     description = "instance_type"
+     default = "t2.nano"
+}
