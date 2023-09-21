814088135f186e2cb0c47065f2e805639b0b0dcd
Prakash Kumar <34745120+prakashk0301@users.noreply.github.com>
2023-09-21 22:33:29 +0530

    Create ubuntu_linux_vm1.tf

---
+++ ubuntu_linux_vm1.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "linux_vm" {
+    ami                         = "${var.aws_ami}"
+    availability_zone           = "eu-central-1a"
+    ebs_optimized               = false
+    instance_type               = "t2.micro"
+    monitoring                  = false
+    key_name                    = "${var.aws_key}"
+    subnet_id                   = "${var.aws_subnet}"
+    source_dest_check = false
+    vpc_security_group_ids      = ["${aws_security_group.sec-grp.id}"]
+    associate_public_ip_address = true
+    user_data = "${file("user-data.sh")}"
+    root_block_device {
+        volume_type           = "gp2"
+        volume_size           = 8
+        delete_on_termination = false
+    }
+    tags = {
+        Name = "linux_vm"
+    }
+}
