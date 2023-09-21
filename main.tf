2ac43d13f0213599fa166dae218793a40d7ef138
Prakash Kumar <34745120+prakashk0301@users.noreply.github.com>
2023-09-21 23:06:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,53 +3,53 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-resource "aws_vpc" "some_custom_vpc" {
+resource "aws_vpc" "my_custom_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Some Custom VPC"
+    Name = "my Custom VPC"
   }
 }
 
-resource "aws_subnet" "some_public_subnet" {
-  vpc_id            = aws_vpc.some_custom_vpc.id
+resource "aws_subnet" "my_public_subnet1" {
+  vpc_id            = aws_vpc.my_custom_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-central-1a"
 
   tags = {
-    Name = "Some Public Subnet"
+    Name = "my Public Subnet1"
   }
 }
 
-resource "aws_subnet" "some_private_subnet" {
-  vpc_id            = aws_vpc.some_custom_vpc.id
+resource "aws_subnet" "my_public_subnet2" {
+  vpc_id            = aws_vpc.my_custom_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-central-1b"
 
   tags = {
-    Name = "Some Private Subnet"
+    Name = "my Public Subnet2"
   }
 }
 
-resource "aws_internet_gateway" "some_ig" {
-  vpc_id = aws_vpc.some_custom_vpc.id
+resource "aws_internet_gateway" "my_ig" {
+  vpc_id = aws_vpc.my_custom_vpc.id
 
   tags = {
-    Name = "Some Internet Gateway"
+    Name = "my Internet Gateway"
   }
 }
 
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.some_custom_vpc.id
+  vpc_id = aws_vpc.my_custom_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.some_ig.id
+    gateway_id = aws_internet_gateway.my_ig.id
   }
 
   route {
     ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.some_ig.id
+    gateway_id      = aws_internet_gateway.my_ig.id
   }
 
   tags = {
@@ -58,13 +58,13 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public_1_rt_a" {
-  subnet_id      = aws_subnet.some_public_subnet.id
+  subnet_id      = [ "aws_subnet.my_public_subnet1.id" , "aws_subnet.my_public_subnet2.id" ]
   route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_security_group" "web_sg" {
-  name   = "HTTP and SSH"
-  vpc_id = aws_vpc.some_custom_vpc.id
+  name   = "HTTP , HTTPS, and SSH"
+  vpc_id = aws_vpc.my_custom_vpc.id
 
   ingress {
     from_port   = 80
@@ -78,6 +78,12 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
@@ -87,25 +93,46 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-resource "aws_instance" "web_instance" {
-  ami           = "ami-0533f2ba8a1995cf9"
+resource "aws_instance" "web_instance1" {
+  ami           = "ami-04e601abe3e1a910f"
   instance_type = "t2.nano"
-  key_name      = "MyKeyPair2"
+  key_name      = "devops-june"
 
-  subnet_id                   = aws_subnet.some_public_subnet.id
+  subnet_id                   = aws_subnet.my_public_subnet1.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
   user_data = <<-EOF
   #!/bin/bash -ex
 
-  amazon-linux-extras install nginx1 -y
-  echo "<h1>$(curl https://api.kanye.rest/?format=text)</h1>" >  /usr/share/nginx/html/index.html 
-  systemctl enable nginx
-  systemctl start nginx
-  EOF
+  sudo apt-get update -y
+  sudo apt-get install apache2 -y
+  sudo systemctl start apache2
+  sudo systemctl enable apache2
 
   tags = {
-    "Name" : "Kanye"
+    "Name" : "web_instance1"
+  }
+}
+
+resource "aws_instance" "web_instance2" {
+  ami           = "ami-04e601abe3e1a910f"
+  instance_type = "t2.nano"
+  key_name      = "devops-june"
+
+  subnet_id                   = aws_subnet.my_public_subnet2.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+  #!/bin/bash -ex
+
+  sudo apt-get update -y
+  sudo apt-get install apache2 -y
+  sudo systemctl start apache2
+  sudo systemctl enable apache2
+
+  tags = {
+    "Name" : "web_instance2"
   }
 }
